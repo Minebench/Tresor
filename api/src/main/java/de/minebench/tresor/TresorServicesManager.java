8fb68c5c0fc9f3f8dd91e23d9afd1310bc1d84d0
Phoenix616 <mail@moep.tv>
2020-05-04 16:11:11 +0100

    Clarify license situation. The actual plugin is GPLv3, api is LGPLv3.

--- api/src/main/java/de/minebench/tresor/TresorServicesManager.java
+++ api/src/main/java/de/minebench/tresor/TresorServicesManager.java
@@ -7,6 +7,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collection;
 
+/*
+ * Tresor - Abstraction library for Bukkit plugins
+ * Copyright (C) 2020 Max Lee (https://github.com/Phoenix616)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tresor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public interface TresorServicesManager extends ServicesManager {
     
     /**
