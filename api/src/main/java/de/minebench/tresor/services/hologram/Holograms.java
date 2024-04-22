ed8d93be0f646568b0cb32ac40af1a02728ca89d
Phoenix616 <max@themoep.de>
2024-04-22 21:27:39 +0100

    Add comments

--- api/src/main/java/de/minebench/tresor/services/hologram/Holograms.java
+++ api/src/main/java/de/minebench/tresor/services/hologram/Holograms.java
@@ -1,16 +1,62 @@
 package de.minebench.tresor.services.hologram;
 
+/*
+ * Tresor - Abstraction library for Bukkit plugins
+ * Copyright (C) 2024 Max Lee aka Phoenix616 (max@themoep.de)
+ * Copyright (C) 2024 Tresor Contributors (https://github.com/Minebench/Tresor/graphs/contributors)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tresor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.minebench.tresor.services.TresorServiceProvider;
 import org.bukkit.Location;
 
+/**
+ * The holograms service API for managing {@link Hologram}s
+
+ */
 public interface Holograms extends TresorServiceProvider {
 
+    /**
+     * Create a hologram at the given location
+     * @param hologramId The id of the hologram
+     * @param location   The location to create the hologram at
+     * @return The created hologram
+     */
     Hologram createHologram(String hologramId, Location location);
+
+    /**
+     * Get a hologram by its id
+     * @param hologramId The id of the hologram
+     * @return The hologram or null if it doesn't exist
+     */
     Hologram getHologram(String hologramId);
 
+    /**
+     * Check if the provider supports a certain feature
+     * @param feature The feature to check for
+     * @return Whether the feature is supported
+     */
     boolean supports(Feature feature);
 
+    /**
+     * The features that can be supported by a hologram provider
+     */
     enum Feature {
+        /**
+         * Holograms can be toggled per player
+         */
         PER_PLAYER,
     }
 }
--- providers/decentholograms/src/main/java/de/minebench/tresor/providers/decentholograms/DecentHologramWrapper.java
+++ providers/decentholograms/src/main/java/de/minebench/tresor/providers/decentholograms/DecentHologramWrapper.java
@@ -1,5 +1,24 @@
 package de.minebench.tresor.providers.decentholograms;
 
+/*
+ * Tresor - Abstraction library for Bukkit plugins
+ * Copyright (C) 2024 Max Lee aka Phoenix616 (max@themoep.de)
+ * Copyright (C) 2024 Tresor Contributors (https://github.com/Minebench/Tresor/graphs/contributors)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tresor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.minebench.tresor.services.hologram.Hologram;
 import de.minebench.tresor.services.hologram.Holograms;
 import de.minebench.tresor.services.hologram.Holograms.Feature;
